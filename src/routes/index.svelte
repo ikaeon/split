f29f51c86af75a3b3b42ca0267fef3aa0799d39c
ikaeon@github.com <ikaeon@github.com>
2020-07-06 22:18:34 +1000

    mutliplayer support

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,29 +4,62 @@
 
   let dimi = 100; 
 	let canvas;
-	let handle_keydown;
-	let fps = 50;
-	let cell_no= 50;
-	let board_state = Array(cell_no).fill(0).map(x => Array(cell_no).fill(0));
-	let head_x = 0;
-	let head_y = 25;
-	let head_dx = 1;
-	let head_dy = 0;
-	
+	let fps = 20;
+	let board_state = Array(66).fill(0).map(x => Array(66).fill(0));
 	let lines = false;
+	let cell_no = 50;
+	let clear = false;
+	let playing = false;
+
 
 	const socket = io();
 	
 	socket.on('whoami', function(msg) {
-		console.log("heheh",msg);
 		lines = msg;
 	});
 
+	socket.on('update_board',(u) => {
+		u.forEach(([x,y,v]) => board_state[x][y] = v);
+	});
 
+
+	socket.on('start',() => {
+		clear = true;
+		playing = true;
+		board_state = Array(66).fill(0).map(x => Array(66).fill(0));
+	});
+	
 	$: grid = Math.floor(dimi / cell_no) ;
 	$: dim = grid * cell_no;
-	$: scale = (cell) => grid * cell;
-  
+	$: scale = (cell) => grid * (cell - 1);
+
+	function b_allowed(x,y) {
+		
+		return board_state[x][y] == 0;
+
+	}
+
+	function handle_click(e) {
+		var rect = canvas.getBoundingClientRect();
+		const x = 1+Math.floor((e.clientX - rect.left) / grid);
+    const y = 1+Math.floor((e.clientY - rect.top) / grid);
+		if(b_allowed(x,y)) socket.emit('place_bollard',x,y);
+		
+	}
+
+	 function handle_keydown(e) {
+		socket.emit('change_direction',e.which);
+	}
+
+	function handle_start() {
+		socket.emit('start');
+	}
+
+	function handle_stop() {
+		socket.emit('stop');
+		playing = false;
+	}
+
 	onMount(() => {
 		
 		dimi = Math.min(canvas.parentElement.clientHeight,
@@ -34,126 +67,37 @@
 
 		var context = canvas.getContext('2d');
 
-		var count = 0;
 
-		var snake = {
-			x: 0,
-			y: Math.floor(dimi / 2),
-			
-			// snake velocity. moves one grid length every frame in either the x or y direction
-			dx: grid,
-			dy: 0,
-			
-			// keep track of all grids the snake body occupies
-			cells: [],
-			
-			// length of the snake. grows when eating an apple
-			maxCells: 4
-		};
-		
-		var apple = {
-			x: scale(Math.floor(cell_no / 2)),
-			y: scale(Math.floor(cell_no / 2))
-		};
-
-		// get random whole numbers in a specific range
-		// @see https://stackoverflow.com/a/1527820/2124254
-		function getRandomInt(min, max) {
-			return Math.floor(Math.random() * (max - min)) + min;
-		}
-
-		handle_keydown = function(e) {
-			// prevent snake from backtracking on itself by checking that it's 
-			// not already moving on the same axis (pressing left while moving
-			// left won't do anything, and pressing right while moving left
-			// shouldn't let you collide with your own body)
-			// left arrow key
-			if (e.which === 37 && head_dx === 0) {
-				head_dx = -1;
-				head_dy = 0;
-			}
-			// up arrow key
-			else if (e.which === 38 && head_dy === 0) {
-				head_dx = 0;
-				head_dy = -1;
-			}
-			// right arrow key
-			else if (e.which === 39 && head_dx === 0) {
-				head_dx = 1;
-				head_dy = 0;
-			}
-			// down arrow key
-			else if (e.which === 40 && head_dy === 0) {
-				head_dx = 0;
-				head_dy = 1;
-			}
-		}
-		socket.on('loop',()=> loop());
 		// game loop
 		function loop() {
-			//requestAnimationFrame(loop);
-		
-			// slow game loop to 15 fps instead of 60 (60/15 = 4)
-			if (++count < (61 - fps)) {
-				return;
+			requestAnimationFrame(loop);
+			if(clear) {
+				context.clearRect(0,0,canvas.width,canvas.height);
+				clear = false;
 			}
 
-			count = 0;
-//			context.clearRect(0,0,canvas.width,canvas.height);
-			
-			for(let x = 0; x < cell_no; ++x) {
-				for(let y = 0; y < cell_no; ++y) {
+			for(let x = 1; x <= cell_no; ++x) {
+				for(let y = 1; y <= cell_no; ++y) {
 					if(board_state[x][y] == 1) {
 						context.fillStyle = 'green';
 						context.fillRect(scale(x), scale(y), grid-1, grid-1);  
 					}
 					
-					if(board_state[x][y] == 2) {
+					else if(board_state[x][y] == 2) {
 						context.fillStyle = 'red';
 						context.fillRect(scale(x), scale(y), grid, grid);  
 					}
 
+					else if(board_state[x][y] == 3) {
+						context.fillStyle = 'pink';
+						context.fillRect(scale(x), scale(y), grid-1, grid-1);  
+					}
+
 				}
 			}
-
-
-			// move snake by it's velocity
-			head_x += head_dx;
-			head_y += head_dy;
-			
-			if(head_x >= cell_no || head_y >= cell_no) {
-				//round over
-			}else if(board_state[head_x][head_y] == 1) {
-				//round over
-			}else if(board_state[head_x][head_y] == 2) {
-				let dir = Math.random() < 0.5 ? -1 : 1;
-				let tx = head_dx*dir;
-				let ty = head_dy*dir;
-				
-				head_x -= head_dx;
-				head_y -= head_dy;
-				head_dx = ty;
-				head_dy = tx;
-				
-				head_x += head_dx;
-				head_y += head_dy;
-			
-				board_state[head_x][head_y] = 1;
-			}else {
-				board_state[head_x][head_y] = 1;
-			}
-
-			// draw apple
-			loop: do {
-				let x = getRandomInt(0,cell_no);
-				let y = getRandomInt(0,cell_no);
-
-				if (board_state[x][y] != 0) continue loop;
-				board_state[x][y] = 2; 
-			} while(0);
 		}
 
-		//requestAnimationFrame(loop);
+		requestAnimationFrame(loop);
 	});
 
 
@@ -184,6 +128,7 @@
 		justify-content: stretch;
 		align-content: center;
 		height:100%;
+		width:200px;
 					
 	}
 
@@ -194,18 +139,23 @@
 
 </style>
 
+
 <svelte:window on:keydown={handle_keydown}/>
 
 <main>
 
 <h1> SPLIT </h1>
 
-
 <controlarea>
-<p>Snake Speed: {fps} </p>
+{#if lines}
+<p> Control Line</p>
+{:else}
+<p> Conrol Chess</p>
+{/if}
+<p>Grid/s:  {fps} </p>
 
 <label>
-	<input type=range bind:value={fps} min=1 max=60 onkeydown="return false">
+	<input type=range bind:value={fps} min=1 max=64 onkeydown="return false">
 </label>
 
 <p>Dimensions: {cell_no}   </p>
@@ -213,6 +163,16 @@
 <label>
 	<input type=range bind:value={cell_no} min=8 max=64 onkeydown="return false">
 </label>
+
+{#if playing}
+<button on:click={handle_stop}>
+    Stop
+</button>
+{:else}
+<button on:click={handle_start}>
+    Start
+</button>
+{/if}
 
 
 </controlarea>
@@ -222,6 +182,7 @@
 	bind:this={canvas}
 	width={dim}
 	height={dim}
+	on:click={handle_click}
 ></canvas>
 
 </main>
--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import compression from 'compression';
 import * as sapper from '@sapper/server';
 import http from 'http';
 import sock from 'socket.io';
-//import worker from 'worker_threads';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -22,16 +21,113 @@ polka({server}) // You can also use Express
 
 const io = sock(server);
 
+const max_dim = 64 + 2;
 let who = 0;
+let loop_hande;
+let board_state = Array(max_dim).fill(0).map(x => Array(max_dim).fill(0));
+let head_x = 1,head_y = 1,h_dx = 1,h_dy = 0;
+let board_dim = 50;
 
-//let h = new worker.Worker('./test.js');
 
+function update_board(u) {
+	const f = u.filter(([x,y,v]) => board_state[x][y] == 0 || board_state[x][y] == 3); 
+
+	f.forEach(([x,y,v]) => {board_state[x][y] = v;});
+	io.emit('update_board',f);
+}
+
+function main_loop() {
+		
+
+	const nx = head_x + h_dx;
+	const ny = head_y + h_dy;
+	
+	if(head_x > board_dim || 
+		head_y > board_dim ||
+		head_x < 1 || 
+		head_y < 1) {
+		clearInterval(loop_hande);
+		// Edge of board
+		// game over
+
+	}else if(board_state[nx][ny] == 1) {
+		// game over
+		clearInterval(loop_hande);
+	}else if(board_state[nx][ny] == 2) {
+		let dir = Math.random() < 0.5 ? -1 : 1;
+		let tx = h_dx*dir;
+		let ty = h_dy*dir;
+		
+		h_dx = ty;
+		h_dy = tx;
+		
+		head_x += h_dx;
+		head_y += h_dy;
+		update_board([[head_x,head_y,1]]);
+	}else {
+		head_x = nx;
+		head_y = ny;
+		update_board([[nx,ny,1]]);
+	}
+}
+
+
+function change_direction(e) {
+	// left arrow key
+	if (e === 37 && h_dx === 0) {
+		if(board_state[head_x-1][head_y] != 2) {h_dx=-1;h_dy=0;}
+	}
+	// up arrow key
+	else if (e === 38 && h_dy === 0) {
+		if(board_state[head_x][head_y+1] != 2) {h_dx=0,h_dy=-1};
+	}
+	// right arrow key
+	else if (e === 39 && h_dx === 0) {
+		if(board_state[head_x+1][head_y] != 2) {h_dx=1,h_dy=0}
+	}
+	// down arrow key
+	else if (e === 40 && h_dy === 0) {
+		if(board_state[head_x][head_y+1] != 2) {h_dx=0;h_dy=1};
+	}
+}
 
 io.on('connection', (socket) => {
 
 	socket.emit('whoami', who == 0);
 	who = (who + 1) % 2;
-  setInterval(()=>socket.emit('loop'),1);
+
+	socket.on('head',(dx,dy)=>{
+		io.emit('update_head',dx,dy);
+	});
+
+	socket.on('place_bollard',(x,y)=> {
+	  if(x>=0 && x<=board_dim && y >= 0 && y <=board_dim && board_state[x][y] == 0) {
+			update_board([[x,y,2],[x+1,y+1,3],[x-1,y+1,3],[x+1,y-1,3],[x-1,y-1,3]]);
+		}
+	});
+
+	socket.on('change_direction',change_direction);
+
+	socket.on('start',() =>{
+		io.emit('start');
+		head_x = 1;head_y = 1;h_dx = 1;h_dy = 0;
+		board_state = Array(max_dim).fill(0).map(x => Array(max_dim).fill(0));
+		update_board([[head_x,head_y,1]]);
+		loop_hande = setInterval(main_loop,200);
+	});
+
+	socket.on('stop',()=> {
+		clearInterval(loop_hande);
+	});
+
+	socket.on('change_loop_speed',(s) => {
+		clearInterval(loop_hande);
+    loop_hande = setInterval(socket.emit('loop'),1/s);
+	});
+
+	socket.on('disconnect', function() {
+		clearInterval(loop_hande);
+  });
 
 
 });
